09549e1520d9113504d6afd4610979d70393eda4
Derek Chen <derekgc@icloud.com>
2018-05-21 15:49:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
+
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  validate :better_title
+
+    def better_title
+      if(title == nil)
+        errors.add(:title, "is empty.")
+      elsif !title.include?("Won't Believe")
+        errors.add(:title, "is not good enough")
+      else
+      end
+    end
 end
